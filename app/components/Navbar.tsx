26dd4604e36020d68bb7528e2f0ed52d2798eeaf
heet36 <heet3456@gmail.com>
2025-08-29 22:57:33 +0530

    Updated features / bug fixes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -69,13 +69,7 @@ const Navbar = () => {
                 <div className="flex items-center gap-8">
                     <Link to="/" className="flex items-center gap-2">
                         <p 
-                            className="text-2xl font-bold"
-                            style={{
-                                background: 'linear-gradient(to right, #AB8C95, #000000, #8E97C5)',
-                                WebkitBackgroundClip: 'text',
-                                WebkitTextFillColor: 'transparent',
-                                backgroundClip: 'text'
-                            }}
+                            className="text-2xl font-bold text-gradient"
                         >
                             RESUMIND
                         </p>
